9070a3b6b7468bac6ff8ac45acdc215d012c813f
Sobarinetech <sobarinetechno@gmail.com>
2025-02-10 01:31:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,9 +76,8 @@ st.title("Inboxify by EverTech")
 
 # Additional Information
 st.write("""
-**For Inboxify access, please visit [Amazon Appstore](https://www.amazon.com/gp/product/B0DV3S92JY)**  
-This version is created solely for **open source basis**.  
-The original application is capable of handling **50,000+ email IDs at once**.
+**For Inboxify App, please visit [Amazon Appstore](https://www.amazon.com/gp/product/B0DV3S92JY)**  
+This is version of Inboxify Pro
 """)
 
 # Blacklist upload
